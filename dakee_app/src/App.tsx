5a2d01cca099e690b90b932c4bc9ef64d0076886
InnaKlymovych <98186099+InnaKlymovych@users.noreply.github.com>
2023-04-10 16:23:34 -0600

    adding

--- dakee_app/src/App.tsx
+++ dakee_app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Routes , Route, Router, BrowserRouter } from 'react-router-dom'
+import { Routes , Route,  BrowserRouter } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/home';
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
 import TutorialPage from './pages/tutorial';
+import TutorialbPage from './pages/tutorialb';
+import TutorialcPage from './pages/tutorialb';
 import QuestionairePage from './pages/questionaire';
 
 
@@ -18,6 +20,8 @@ const Application: React.FunctionComponent<IApplicationProps> = (props) => {
       <Route path='login' element={ <LoginPage />} />
       <Route path='signup' element={ <SignupPage />} />
       <Route path='tutorial' element={ <TutorialPage />} />
+      <Route path='tutorialb' element={ <TutorialbPage />} />
+      <Route path='tutorialc' element={ <TutorialcPage />} />
       <Route path='questionaire' element={ <QuestionairePage />} />
     </Routes>
   </BrowserRouter>
--- dakee_app/src/pages/tutorialc.tsx
+++ dakee_app/src/pages/tutorialc.tsx
@@ -1,36 +1,35 @@
 import React from 'react';
 import {MouseEvent} from 'react';
 import logo_small from '../images/icons/logo_small.png';
-import friends from '../images/icons/user-friends.svg';
+import vectorUp from '../images/icons/Vector (1).png';
 import ellipse1 from '../images/icons/Ellipse 1.png'
 import ellipse2 from '../images/icons/Ellipse 2.png'
 
-export interface ITutorialPageProps {};
+export interface ITutorialcPageProps {};
 
-const TutorialPage: React.FunctionComponent<ITutorialPageProps> = props => {
+const TutorialcPage: React.FunctionComponent<ITutorialcPageProps> = props => {
 
    const handleClick = (event: MouseEvent) => {
-      window.location.href="tutorialb"
+      window.location.href="tutoriald"
    }
-   
    return (
-   <div onClick={handleClick} className='tutorial_friends' id='tutorial_friends' >
+   <div onClick={handleClick} className='tutorial_up' id='tutorial_up' >
       <img src={logo_small} alt="logo" />
-      <div className="tutorial_box">
-         <div className="tutorial_text1">
+      <div className="tutorial_c_box">
+         <div className="tutorial_c_text1">
             <h2>Hi, Inna! <br /> Here´s a short <br />tutorial for you:</h2>
          </div>
-         <img src={friends} alt="friends" />
-         <div className="tutorial_text2">
+         <img src={vectorUp} alt="vectorUp" />
+         <div className="tutorial_c_text2">
             <p>Dakee was created to give the real, non-touristic experience in a visited city. 
                Based on your interests, we connect Travellers and Locals to exchange 
                ideas and give tips on what is actually worth to do and see. No tourist cliché!
             </p>
          </div>
          <div className="ellipse">
+            <img src={ellipse2} alt="ellipse2" />            
+            <img src={ellipse2} alt="ellipse2" />
             <img src={ellipse1} alt="ellipse1" />
-            <img src={ellipse2} alt="ellipse2" />
-            <img src={ellipse2} alt="ellipse2" />
             <img src={ellipse2} alt="ellipse2" />
          </div>
          <p>Scip the tutorial</p>
@@ -41,4 +40,4 @@ const TutorialPage: React.FunctionComponent<ITutorialPageProps> = props => {
    );
 }
 
-export default TutorialPage;
+export default TutorialcPage;
